7ca074c29c8edcd342eacdf0b502c869a7fcf1dc
Trent Patterson <jtrent238@outlook.com>
2016-10-04 18:00:41 -0400

    Update logger.js

--- js/logger.js
+++ js/logger.js
@@ -1,1 +1,4 @@
+window.alert("*********************************************************");
+window.alert("This site uses JavaScript Website Logger By: jtrent238");
+window.alert("*********************************************************");
 console.log("*******************Console Log Started!*******************");
