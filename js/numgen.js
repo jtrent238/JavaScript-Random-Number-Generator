fe816db52e7ed5e37752e67f7fa1115c0d5e7d3e
Trent Patterson <jtrent238@outlook.com>
2016-10-05 09:08:31 -0400

    Update numgen.js

--- js/numgen.js
+++ js/numgen.js
@@ -1,3 +1,3 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = Math.floor(Math.random() * 11 * 2 * Math.random() * 21 * Math.random() * 22); // returns a random number between 0 and 10
+    document.getElementById("demo").innerHTML = Math.floor(Math.random() * 11 * 2 * Math.random() * 21 * Math.random() * 22); // returns a random number
 }
