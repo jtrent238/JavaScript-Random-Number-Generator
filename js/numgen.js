de962e8c10616399f7d623dae4416e0930d8f869
Trent Patterson <jtrent238@outlook.com>
2018-08-20 17:21:40 -0400

    Update numgen.js

--- js/numgen.js
+++ js/numgen.js
@@ -1,5 +1,5 @@
 function myFunction() {
     var max = document.getElementById("max").innerHTML;
     var min = document.getElementById("min").innerHTML;
-    document.getElementById("demo").innerHTML = Math.floor(Math.random() * max) + min;//Math.floor(Math.random() * 11 * 2 * Math.random() * 21 * Math.random() * 22); // returns a random number
+    document.getElementById("demo").innerHTML = Math.floor(Math.random() * 11 * 2 * Math.random() * 21 * Math.random() * 22); // returns a random number
 }
